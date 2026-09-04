973dd7c27ae405eeaf0a296931ae882df3cae8d1
Israt Jahan <eraisrat022@gmail.com>
2026-09-04 21:11:47 +0600

    add dns code (#14)

--- handyhub-client/src/lib/auth.ts
+++ handyhub-client/src/lib/auth.ts
@@ -4,7 +4,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGO_DB_URI!);
 const db = client.db(process.env.AUTH_DB_NAME);
-
+console.log(process.env.MONGO_DB_URI)
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,

7d5147498fe57db21f136f8f9c9920817c7163ac
shantodey <ratul3330@gmail.com>
2026-09-15 00:14:02 +0600

    feat: update admin page header to improve clarity

--- handyhub-client/src/app/(dashboard)/dashboard/admin/page.tsx
+++ handyhub-client/src/app/(dashboard)/dashboard/admin/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            Admin Home page By Shanto Dey
+            <h1> hi this is admin page</h1>
         </div>
     );
 };

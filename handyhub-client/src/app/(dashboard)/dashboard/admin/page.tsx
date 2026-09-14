95dc049c3f7261d5e9f66d8eb9c6c927bfd05080
shantodey <ratul3330@gmail.com>
2026-09-15 00:16:25 +0600

    ff

--- handyhub-client/src/app/(dashboard)/dashboard/admin/page.tsx
+++ handyhub-client/src/app/(dashboard)/dashboard/admin/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
     return (
         <div>
             <h1> hi this is admin page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi consectetur explicabo facere repellat id sit dignissimos sint recusandae vero, commodi ad at velit pariatur maiores nemo distinctio temporibus ipsa nobis?</p>
         </div>
     );
 };

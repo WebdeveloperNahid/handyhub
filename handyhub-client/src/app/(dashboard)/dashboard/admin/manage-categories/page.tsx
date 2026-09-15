bf04d724f808527c295a3af2e15b01386283ce2e
Shanto Dey <126257294+shantodey@users.noreply.github.com>
2026-09-15 06:06:14 +0600

    Admin (#33)
    * chore: add pnpm workspace configuration to enable esbuild builds
    
    * ll
    
    * feat: update admin page header to improve clarity
    
    * ff
    
    * made Pagination table with demo data for user and provider

--- handyhub-client/src/app/(dashboard)/dashboard/admin/manage-categories/page.tsx
+++ handyhub-client/src/app/(dashboard)/dashboard/admin/manage-categories/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 
 const page = () => {
     return (
         <div>
-            manage-categories page
+            manage-categories page by shanto dey 
         </div>
     );
 };
--- handyhub-client/src/app/(dashboard)/dashboard/admin/page.tsx
+++ handyhub-client/src/app/(dashboard)/dashboard/admin/page.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            Admin Home page
+            <h1> hi this is admin page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi consectetur explicabo facere repellat id sit dignissimos sint recusandae vero, commodi ad at velit pariatur maiores nemo distinctio temporibus ipsa nobis?</p>
         </div>
     );
 };

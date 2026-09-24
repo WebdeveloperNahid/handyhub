6b90197989f9ef36b8999097b7cbc9fdde4534ca
WebdeveloperNahid <omarfaruk.nahid.webdeveloper@gmail.com>
2026-09-24 19:25:32 +0600

    update dashboard sidebar, provider profile and remove manage categories

--- handyhub-client/src/app/(dashboard)/dashboard/provider/profile/page.tsx
+++ handyhub-client/src/app/(dashboard)/dashboard/provider/profile/page.tsx
@@ -2,10 +2,12 @@ import ProfilePage from "@/Components/dashboard/shared/Profile/ProfilePage";
 
 const Profile = () => {
   return (
+
     <div>
       <ProfilePage />
     </div>
   )
+  
 }
 
 export default Profile;
